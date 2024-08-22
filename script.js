b5456107f3d50c7f1fcd32555f54f54d3494f64b
Tomi Alaofin <Alaofin.tomi@gmail.com>
2024-08-22 16:58:34 -0500

    added delete functionality to the app

--- script.js
+++ script.js
@@ -3,18 +3,28 @@
 //get text written within  the box
 //stack it 
 const getinput = document.getElementById('add-todo-btn');
+const todoInput = document.getElementById('todo-input');
+const todoList = document.getElementById('todo-list')
 getinput.addEventListener('click', displayTask);
+
 function displayTask(){
-    const inputField = document.getElementById('todo-input');
-    const inputValue =inputField.value;
-    if(inputValue.trim() === ''){
+    const inputValue =todoInput.value.trim();
+    if(inputValue === ''){
         alert('Enter a task');
     }
     else{
         const list = document.createElement('li');
         list.textContent = inputValue;
-        const todoList = document.getElementById('todo-list')
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.id= 'deleteButton';
+        deleteButton.addEventListener('click', () => removeTask(list));
+        list.appendChild(deleteButton);
         todoList.appendChild(list);
+        todoInput.value = ''; // clear input field
     }
-
 }
+function removeTask(listItem){
+    listItem.remove();
+}
+//when a new task is added, add a delete option
